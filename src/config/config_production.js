9e36d590eb3ee12e2b21c03167033a92b4b8dabd
PIUSH KUMAR SINGH <singhpishusingh@gmail.com>
2020-05-30 12:46:52 +0530

    Create config_production.js

---
+++ src/config/config_production.js
@@ -0,0 +1,9 @@
+const apiUrl={
+    "getReport":{
+        'usage':'get world covid report according to given date',
+        'verb':'GET',
+        'URL':'https://covidrep2019.herokuapp.com/items/<date>'
+    }
+}
+
+export default apiUrl
